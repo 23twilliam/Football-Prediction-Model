c93426246747794d2a8e3000d6143af3b0e5e318
23twilliam <172639957+23twilliam@users.noreply.github.com>
2026-08-01 23:52:18 +0100

    Initial setup + basic data loading. To add aggregate of Odds

---
+++ src/data.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import pandas as pd
+
+SCHEMA_COLUMN_MAP = {
+    "Date": "date",
+    "HomeTeam": "home_team",
+    "AwayTeam": "away_team",
+    "FTHG": "home_goals",
+    "FTAG": "away_goals",
+
+    "B365H": "decision_odds_home",
+    "B365D": "decision_odds_draw",
+    "B365A": "decision_odds_away",
+
+    "B365CH": "closing_odds_home",
+    "B365CD": "closing_odds_draw",
+    "B365CA": "closing_odds_away",
+}
+
+def load_football_data_csv(path: Path, season: str):
+    raw = pd.read_csv(path)
+    raw['Date'] = pd.to_datetime(raw['Date'], dayfirst=True)
+    raw = raw[list(SCHEMA_COLUMN_MAP.keys())]
+    raw["season"] = season
+    renamed = raw.rename(columns=SCHEMA_COLUMN_MAP)
+    return renamed
+
+if __name__ == '__main__':
+    dataframe = load_football_data_csv(Path('../data/E0.csv') ,'24/25')
+    print(dataframe.head())
