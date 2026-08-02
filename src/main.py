95a6ebfb2cb8ed65cd2b61954ee8e0bc8fe9f394
23twilliam <172639957+23twilliam@users.noreply.github.com>
2026-08-02 21:06:39 +0100

    Tau correction added to prob calc

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 from pathlib import Path
+import numpy as np
 from data import load_football_data_csv
-from poisson_model import simple_match_probs, fit_attack_defence
-import pandas as pd
+from poisson_model import simple_match_probs, fit_attack_defence, tau_correction
 
 matches = load_football_data_csv(Path('../data/E0.csv'), '24/25')
 
 lam = matches['home_goals'].mean()
 mu = matches['away_goals'].mean()
 
+# probs = simple_match_probs(lam, mu)
+
+teams, attack, defense, home_adv, rho = fit_attack_defence(matches)
+i = teams.index("Nott'm Forest")
+j = teams.index("Man United")
+lam = np.exp(attack[i] - defense[j] + home_adv)
+mu = np.exp(attack[j] - defense[i])
+
 probs = simple_match_probs(lam, mu)
-toy_matches = pd.DataFrame([
-    {'home_team': 'A', 'away_team': 'B', 'home_goals': 3, 'away_goals': 1},
-    {'home_team': 'A', 'away_team': 'C', 'home_goals': 2, 'away_goals': 0},
-    {'home_team': 'A', 'away_team': 'D', 'home_goals': 4, 'away_goals': 0},
-    {'home_team': 'B', 'away_team': 'C', 'home_goals': 1, 'away_goals': 1},
-    {'home_team': 'B', 'away_team': 'D', 'home_goals': 2, 'away_goals': 1},
-    {'home_team': 'C', 'away_team': 'D', 'home_goals': 1, 'away_goals': 0},
-])
-print(fit_attack_defence(toy_matches))
+print(probs)
--- src/poisson_model.py
+++ src/poisson_model.py
@@ -4,6 +4,22 @@ import pandas as pd
 import scipy.optimize as optimize
 
 
+def tau_correction(x, y, lam, mu, rho):
+    tau = np.ones_like(lam)
+
+    m00 = (x == 0) & (y == 0)
+    m01 = (x == 0) & (y == 1)
+    m11 = (x == 1) & (y == 1)
+    m10 = (x == 1) & (y == 0)
+
+    tau[m00] = 1 - lam[m00] * mu[m00] * rho
+    tau[m01] = 1 + lam[m01] * rho
+    tau[m10] = 1 + lam[m10] * rho
+    tau[m11] = 1 - rho
+
+    return tau
+
+
 def fit_attack_defence(matches_df: pd.DataFrame, l2_reg: int = 0.02):
     teams = sorted(pd.concat([matches_df["home_team"], matches_df["away_team"]]).unique())
     team_to_idx = {team: i for i, team in enumerate(teams)}
@@ -18,19 +34,21 @@ def fit_attack_defence(matches_df: pd.DataFrame, l2_reg: int = 0.02):
 
     # Defined INSIDE since scipy.optimize.minimize will only ever call this as nll(params)
     def nll(params):
-        # vector of attack, defence, home_adv.
+        # vector of attack, defence, home_adv, rho
         attack = params[:n_teams]
         defence = params[n_teams: 2 * n_teams]
-        home_adv = params[-1]
+        home_adv = params[-2]
+        rho = params[-1]
 
         # Exp() because of the Log-Linear parameterisation, guarantees lam/mu stay positive no matter what the numbers
         # happen to be
         lam = np.exp(attack[home_idx] - defence[away_idx] + home_adv)  # Home attack vs Away Defence (+ home adv)
-        mu = np.exp(attack[away_idx] - defence[home_idx]) # Opposite
+        mu = np.exp(attack[away_idx] - defence[home_idx])  # Opposite
 
         # Joint probability of the exact observed scoreline for every match. Same formula as poisson, but vectorised
         # across every match's own lam/mu instead of one shared league wide pair
-        p = stats.poisson.pmf(home_goals, lam) * stats.poisson.pmf(away_goals, mu)
+        p = (stats.poisson.pmf(home_goals, lam) * stats.poisson.pmf(away_goals, mu)
+             * tau_correction(home_goals, away_goals, lam, mu, rho))
         # Stops log(0)
         p = np.clip(p, 1e-10, None)
 
@@ -43,16 +61,22 @@ def fit_attack_defence(matches_df: pd.DataFrame, l2_reg: int = 0.02):
 
     # Starting guess, all teams begin "equal" (zero attack, or defence) with a small nudge for home advantage,
     # must be same length as what nll expects above
-    x0 = np.concatenate([np.zeros(n_teams), np.zeros(n_teams), [0.065]])
+    x0 = np.concatenate([np.zeros(n_teams), np.zeros(n_teams), [0.065], [0.0]])
+
+    # Bounds for extremes
+    bounds = [(None, None)] * (2 * n_teams) + [(None, None)], (-0.3, 0.3)
+
     # Walks until nll's gradient is zero.
-    result = optimize.minimize(nll, x0, method='L-BFGS-B')  # quasi-Newton method, uses the gradient of log likelihood.
+    result = optimize.minimize(nll, x0, method='L-BFGS-B', bounds=bounds)  # quasi-Newton method, uses the gradient of
+    # log likelihood.
 
     # Unpack with same slicing as nll
     attack = result.x[:n_teams]
     defence = result.x[n_teams:2 * n_teams]
-    home_adv = result.x[-1]
+    home_adv = result.x[-2]
+    rho = result.x[-1]
 
-    return teams, attack, defence, home_adv
+    return teams, attack, defence, home_adv, rho
 
 
 def simple_match_probs(lam: float, mu: float, max_goals: int = 10) -> dict:
